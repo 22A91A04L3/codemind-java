21ad5bef0d8215e69116d8dae6851ce31ae0842d
22A91A04L3 <120238858+22A91A04L3@users.noreply.github.com>
2023-06-15 00:26:10 +0530

    import java.util.*; public class mutli {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int b=sc.nextInt();         int c=a/b;         System.out.println(c);     } }

--- Division.java
+++ Division.java
@@ -3,7 +3,7 @@ public class mutli
 {
     public static void main(String args[])
     {
-        Scanner sc=new Scanner (System.in);
+        Scanner sc=new Scanner(System.in);
         int a=sc.nextInt();
         int b=sc.nextInt();
         int c=a/b;
