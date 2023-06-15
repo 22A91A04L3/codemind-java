bd16ee5e947d5c5a016dfd3d252786d4bc96cac9
22A91A04L3 <120238858+22A91A04L3@users.noreply.github.com>
2023-06-15 11:07:55 +0530

    import java.util.*;  public class praveen{ public static void main(String args[]){     int r;     double area;     Scanner s=new Scanner(System.in);     r=s.nextInt();     area=3.14*r*r;    System.out.format("%.2f",area); } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,11 @@
+import java.util.*;
+ public class praveen{
+public static void main(String args[]){
+    int r;
+    double area;
+    Scanner s=new Scanner(System.in);
+    r=s.nextInt();
+    area=3.14*r*r;
+   System.out.format("%.2f",area);
+}
+}
